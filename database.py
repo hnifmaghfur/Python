3220c53d661f635f49553117c7c2c8e34b39e105
hnifmaghfur <hnifmaghfur@gmail.com>
2020-01-03 09:51:16 +0700

    Memanggil isi Database dengan menggunakan Python.

---
+++ database.py
@@ -0,0 +1,37 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="",
+    database="proj"
+)
+
+# if db.is_connected():
+#     print("Berhasil Connect !")
+
+cursor = db.cursor()
+sql = """
+        SELECT 
+            project.name,
+            episode.name,
+            status.name,
+            duration
+        FROM 
+            shot 
+        INNER JOIN episode ON shot.episode = episode.season
+        INNER JOIN project ON episode.project = project.id
+        INNER JOIN status ON shot.status = status.id
+        WHERE 
+            shot.status = 2
+        ORDER BY
+            project.name ASC
+        """
+cursor.execute(sql)
+
+result = cursor.fetchall()
+
+print('Nama Project, Episode, Status, Duration')
+for data in result:
+    print(data)
+
